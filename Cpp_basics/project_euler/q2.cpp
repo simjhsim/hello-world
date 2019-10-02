0d807e61b3ac91c6b37cd327def50aa65bd47803
simjhsim <simjhsim1996@gmail.com>
2019-10-01 23:42:03 -0400

    Project Euler Q2_progress1

---
+++ Cpp_basics/project_euler/q2.cpp
@@ -0,0 +1,48 @@
+// q2.cpp
+
+// Problem: Even Fibonacci numbers
+// 
+// By considering the terms in the Fibonacci sequence whose
+// values do not exceed four million, find the sum of the
+// even-valued terms.
+
+// Takes in three arguments n1, n2, b where n1 
+// and n2 are the two beginning terms of the
+// Fibonacci sequence and b is the bound.
+// The main function returns the sum of all
+// terms below the bound that are even.
+
+
+#include <iostream>
+#include <string>
+
+#include "sumFibonacci.h"
+
+int main (int argc, char** argv) 
+{
+    if (argc == 4) 
+    {
+        int n1, n2, b;
+
+        n1 = std::stoi (argv[1]);
+        n2 = std::stoi (argv[2]);
+        b  = std::stoi (argv[3]);
+
+        if (n1 <= 0 || n2 <= 0)
+        {
+            std::cout << "[ERROR] invalid initial values.\n";
+            return -1;
+        }
+        else
+        {
+            return sumFibonacci(n1,n2,b);
+        }
+    }
+    else 
+    {
+        std::cout << "[ERROR] wrong number of arguments.\n";
+
+        return -3;
+    }
+}
+
---
+++ Cpp_basics/project_euler/sumFibonacci.cpp
@@ -0,0 +1,27 @@
+// sumFibonacci.cpp
+
+// sum Fibonacci returns the sum of all terms of the
+// Fibonacci sequence beginning with the ordered pair
+// (alpha, beta) of positive integers that is less
+// than bound.
+int sumFibonacci (int alpha, int beta, int bound)
+{
+    int x,y;
+
+    x = alpha % 2;
+    y = beta  % 2;
+
+    if (x == 0)
+    {
+
+    }
+    else if (y == 0)
+    {
+
+    }
+    else
+    {
+        
+    }
+}
+
