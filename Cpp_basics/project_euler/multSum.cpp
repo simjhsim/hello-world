ef4e2fd83c9b098d8da356081fb879288f64e5c1
simjhsim <simjhsim1996@gmail.com>
2019-10-01 23:39:32 -0400

    Project Euler Setup

---
+++ Cpp_basics/project_euler/multSum.cpp
@@ -0,0 +1,42 @@
+// multSum.cpp
+
+// Takes in three arguments n1, n2, bound and 
+// returns the sum of all multiples of n1 and
+// n2 less than the bound, assuming that n1 and
+// n2 are positive integers
+
+
+#include <iostream>
+#include <string>
+
+#include "multSum.h"
+
+int multSum (int mul, int maxval) 
+{
+    if (mul > maxval)
+    {
+        return 0;
+    }
+
+    int a = maxval / mul;
+
+    return (mul * a * (a + 1)) / 2;
+}
+
+int lcm (int a, int b)
+{
+    if (b == 0)
+    {
+        return 0;
+    }
+
+    int mul = a;
+
+    while (mul%b != 0)
+    {
+        mul += a;
+    }
+
+    return mul;
+}
+
---
+++ Cpp_basics/project_euler/multSum.h
@@ -0,0 +1,16 @@
+// multSum.h
+
+#ifndef MULTSUM_H
+#define MULTSUM_H
+
+// Takes in three arguments n1, n2, bound and 
+// returns the sum of all multiples of n1 and
+// n2 less than the bound, assuming that n1 and
+// n2 are positive integers
+
+
+int multSum (int mul, int maxval);
+
+int lcm (int a, int b);
+
+#endif
